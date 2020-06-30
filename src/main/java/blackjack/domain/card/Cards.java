f29104287a52bd1d8fb69ce58e90b1bd402ca188
kimjunseok <jnunseok@gmail.com>
2020-06-30 16:57:12 +0900

    feat: card 일급 컬렉션

---
+++ src/main/java/blackjack/domain/card/Cards.java
@@ -0,0 +1,17 @@
+package blackjack.domain.card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cards {
+
+    private List<Card> cards;
+
+    private Cards(List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
+    }
+
+    public static Cards from(List<Card> cards) {
+        return new Cards(cards);
+    }
+}
