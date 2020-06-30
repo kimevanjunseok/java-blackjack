adacb4827c4509ae550db16232599f66bd870d36
kimjunseok <jnunseok@gmail.com>
2020-06-30 18:48:26 +0900

    refactor: try-catch

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -5,6 +5,10 @@ import blackjack.controller.GameController;
 public class Application {
     public static void main(String[] args) {
         GameController gameController  = GameController.getInstance();
-        gameController.game();
+        try {
+            gameController.game();
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
     }
 }
