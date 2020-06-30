a6a839b4e4bb8255477a0b4c46318cf8fd3adaf6
kimjunseok <jnunseok@gmail.com>
2020-06-30 18:34:42 +0900

    feat: player에 betting money 주입

--- src/main/java/blackjack/domain/user/Player.java
+++ src/main/java/blackjack/domain/user/Player.java
@@ -1,8 +1,16 @@
 package blackjack.domain.user;
 
+import blackjack.domain.user.bettingmoney.BettingMoney;
+
 public class Player extends User {
+
+    private BettingMoney bettingMoney;
 
     public Player(String name) {
         super(name);
+    }
+
+    public void setBettingMoney(int money) {
+        this.bettingMoney = BettingMoney.from(money);
     }
 }
