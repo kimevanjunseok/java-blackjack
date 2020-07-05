132dcb7c2199489448a6b1b3d5c3c20cb72b1fdb
kimjunseok <jnunseok@gmail.com>
2020-07-05 22:30:45 +0900

    feat: earningRate

--- src/main/java/blackjack/domain/ResultType.java
+++ src/main/java/blackjack/domain/ResultType.java
@@ -6,22 +6,28 @@ import blackjack.domain.user.Player;
 import java.util.function.BiFunction;
 
 public enum ResultType {
-    PLAYER_BLACKJACK_WIN((dealer, player) -> !dealer.isBlackjack() && player.isBlackjack()),
-    DEALER_BLACKJACK_WIN((dealer, player) -> dealer.isBlackjack() && !player.isBlackjack()),
-    BLACKJACK_DRAW((dealer, player) -> dealer.isBlackjack() && player.isBlackjack()),
-    PLAYER_BUST_LOSE((dealer, player) -> !dealer.isBust() && player.isBust()),
-    DEALER_BUST_LOSE((dealer, player) -> dealer.isBust()),
-    PLAYER_WIN((dealer, player) -> player.compareScore(dealer) > 0),
-    PLAYER_DRAW((dealer, player) -> player.compareScore(dealer) == 0),
-    PLAYER_LOSE((dealer, player) -> player.compareScore(dealer) < 0);
+    PLAYER_BLACKJACK_WIN((dealer, player) -> !dealer.isBlackjack() && player.isBlackjack(), 1.5),
+    DEALER_BLACKJACK_WIN((dealer, player) -> dealer.isBlackjack() && !player.isBlackjack(), -1),
+    BLACKJACK_DRAW((dealer, player) -> dealer.isBlackjack() && player.isBlackjack(), 0),
+    PLAYER_BUST_LOSE((dealer, player) -> !dealer.isBust() && player.isBust(), -1),
+    DEALER_BUST_LOSE((dealer, player) -> dealer.isBust(), 1),
+    PLAYER_WIN((dealer, player) -> player.compareScore(dealer) > 0, 1),
+    PLAYER_DRAW((dealer, player) -> player.compareScore(dealer) == 0, 0),
+    PLAYER_LOSE((dealer, player) -> player.compareScore(dealer) < 0, -1);
 
     private final BiFunction<Dealer, Player, Boolean> result;
+    private final double earningRate;
 
-    ResultType(BiFunction<Dealer, Player, Boolean> result) {
+    ResultType(BiFunction<Dealer, Player, Boolean> result, double earningRate) {
         this.result = result;
+        this.earningRate = earningRate;
     }
 
     public boolean apply(Dealer dealer, Player player) {
         return result.apply(dealer, player);
+    }
+
+    public double getEarningRate() {
+        return earningRate;
     }
 }
